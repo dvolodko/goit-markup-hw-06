02d91494d05eab2d3a8ca9184abb64d16054550a
Denys Volodko <babyleejane@gmail.com>
2024-04-27 15:28:50 +0300

    first three section adapted to mobile

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,25 @@ const refs = {
   closeModalBtn: document.querySelector("[data-modal='close']"),
   modalOverlay: document.querySelector("[data-modal='overlay']"),
   body: document.querySelector('body'),
+  openMobileMenuBtn: document.querySelector("[data-modal='mobile-open']"),
+  closeMobileMenuBtn: document.querySelector("[data-modal='mobile-close']"),
+  mobileMenu: document.querySelector('.mobile-header-menu'),
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 refs.modalOverlay.addEventListener('click', closeModal);
 
+refs.openMobileMenuBtn.addEventListener('click', toggleMobileMenu);
+refs.closeMobileMenuBtn.addEventListener('click', toggleMobileMenu);
+
 function toggleModal() {
   refs.modalOverlay.classList.toggle('is-open');
+  refs.body.classList.toggle('no-scroll');
+}
+
+function toggleMobileMenu() {
+  refs.mobileMenu.classList.toggle('is-open');
   refs.body.classList.toggle('no-scroll');
 }
 
